239ece33d3e94daaf73bac03562196b29fde5bdc
Oleksiichuk Mykyta <mykyta.oleks.dev@gmail.com>
2025-08-13 13:14:17 +0300

    feat: set up initial context with cart addition function

---
+++ src/store/CartProvider.jsx
@@ -0,0 +1,7 @@
+import { CartContext } from './cartContext';
+
+const CartProvider = ({ children, value }) => {
+	return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
+};
+
+export default CartProvider;
